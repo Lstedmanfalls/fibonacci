0013899d0d9b430feabdfdc3ba12babdfc2b8395
Lisa Stedman-Falls <lstedmanfalls@gmail.com>
2024-01-15 23:57:22 -0700

    update ecs

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,12 +3,10 @@ provider "aws" {
 }
 
 module "gametime-takehome-parent-respository" {
-  source        = "app.terraform.io/lstedmanfalls/gametime-takehome-parent-respository/aws"
-  version       = "1.0.0"
-  environment   = var.environment
-  az            = var.az
-  region        = var.region
-  project_name  = local.name
-  instance_name = "ec2"
-  num_instances = var.num_instances
+  source       = "app.terraform.io/lstedmanfalls/gametime-takehome-parent-respository/aws"
+  version      = "1.0.0"
+  environment  = var.environment
+  az           = var.az
+  region       = var.region
+  project_name = local.name
 }
